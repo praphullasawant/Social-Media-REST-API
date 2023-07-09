e141ce2441e2f1edf5b1608f6112fcc1d2097e7b
Praphulla Sawant <praphullasawant123@gmail.com>
2023-07-09 11:42:34 +0530

    Updating and Deleting a user is Done

--- models/User.js
+++ models/User.js
@@ -40,6 +40,18 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    desc:{
+        type:String,
+        max:50
+    },
+    from:{
+        type:String,
+        max:50
+    },
+    relationship:{
+        type:Number,
+        enum:[1,2,3]
+    }
   },
   { timeseries: true }
 );
--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get("/register", async (req,res)=>{
         const user = await  newUser.save();
         res.status(200).json(user);
     } catch (error) {
-        console.log(error)
+        res.status(500).json(err)
     }
 });
 
@@ -36,7 +36,7 @@ router.post("/login", async (req,res)=>{
 
         res.status(200).json(user);
     } catch (err) {
-        console.log(err);
+        res.status(500).json(err)
     }
 });
 
